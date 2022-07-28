aafc67642bc07f96cbcf31200578326f44a859f8
loops14 <48114656+loops14@users.noreply.github.com>
2022-07-28 15:33:32 +1000

    Add files via upload

--- Histogram.py
+++ Histogram.py
@@ -4,8 +4,19 @@ import sys
 import string
 pip install matplotlib
 
+
+#Used to split the list variables
 l = list()
 x = list.value()
+k = list.key()
 
-plt.hist(x, bins= list.key())
-plt.show()
+#Create a variable for the histogram
+
+fig, plo = plt.subplots()
+
+plo.hist(x)
+plo.set_title('Total words') #Title
+plo.set_xlabel(k) #Name of strings
+plo.set_ylabel('Frequency') #Amount of words
+
+plt.show() #Show graph
